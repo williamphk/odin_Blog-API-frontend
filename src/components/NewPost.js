c90dccfe9c3f202ca6f97dade24f00f6c5b7a34d
William Poon <willphk@outlook.com>
2023-04-26 14:32:11 -0400

    Implemented POST Post Request

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -7,27 +7,28 @@ import "./NewPost.css";
 const NewPost = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [isPublic, setIsPublic] = useState(false);
   const [submitting, setSubmitting] = useState(false);
-  const { currentUser } = useAuth();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setSubmitting(true);
 
     try {
-      const response = await fetch("http://localhost:3000/newpost", {
+      const response = await fetch("http://localhost:3000/posts", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${localStorage.getItem("token")}`, // Attach the JWT token from localStorage
         },
-        body: JSON.stringify({ title, content }),
+        body: JSON.stringify({ title, content, isPublic }),
       });
 
       if (response.ok) {
         console.log("Post created successfully");
         setTitle("");
         setContent("");
+        setIsPublic(false);
       } else {
         console.log("Failed to create post");
       }
@@ -59,6 +60,15 @@ const NewPost = () => {
             value={content}
             onChange={(e) => setContent(e.target.value)}
             required
+          />
+        </div>
+        <div>
+          <label htmlFor="isPublic"> Public:</label>
+          <input
+            type="checkbox"
+            id="isPublic"
+            checked={isPublic}
+            onChange={(e) => setIsPublic(e.target.checked)}
           />
         </div>
         <button type="submit" disabled={submitting}>
